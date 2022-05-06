09a970f324791086667c5c324fb716f0dce44ecb
Dan Arthur <Daniel.Arthur@cazenovecapital.com>
2022-05-06 14:39:52 +0100

    Cutting down example to reproduce issue

--- components/header.tsx
+++ components/header.tsx
@@ -72,33 +72,8 @@ export default function Header() {
             </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/client">
-              <a>Client</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
             <Link href="/server">
               <a>Server</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/protected">
-              <a>Protected</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/api-example">
-              <a>API</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/admin">
-              <a>Admin</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/me">
-              <a>Me</a>
             </Link>
           </li>
         </ul>
